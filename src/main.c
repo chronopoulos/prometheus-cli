fc670523b6ba0687845f108f1d3d482cfda5a855
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2018-11-12 05:04:48 -0500

    main.c: add parsing for length-prefixed responses, and write results to stdout

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
     // create socket
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("ERROR: could not create socket\n");
+        fprintf(stderr, "ERROR: could not create socket\n");
         return 1;
     }
 
@@ -22,13 +22,13 @@ int main(int argc, char **argv) {
     node_addr.sin_family = AF_INET;
     node_addr.sin_port = htons(50660);
     if(inet_pton(AF_INET, "10.0.5.253", &node_addr.sin_addr) <= 0) {
-        printf("ERROR: invalid address\n");
+        fprintf(stderr, "ERROR: invalid address\n");
         return 1;
     }
 
     // make a connection
     if(connect(sockfd, (struct sockaddr *)&node_addr, sizeof(node_addr)) < 0) {
-        printf("ERROR: connect() failed\n");
+        fprintf(stderr, "ERROR: connect() failed\n");
         return 1;
     }
 
@@ -43,12 +43,23 @@ int main(int argc, char **argv) {
         j += 1;
     }
     msgBuf[j] = 0x00; // null
-    printf("debug: %s\n", msgBuf);
 
     // send the message
     send(sockfd, msgBuf, strlen(msgBuf), MSG_NOSIGNAL);
 
-    // close the socket
+    // read length prefix
+    uint32_t responseLength;
+    recv(sockfd, &responseLength, 4, 0);
+
+    // read response
+    unsigned char *responseBuf = (unsigned char *) malloc(responseLength);
+    recv(sockfd, responseBuf, responseLength, 0);
+
+    // write binary data to stdout
+    write(STDOUT_FILENO, responseBuf, responseLength);
+
+    // clean up
+    free(responseBuf);
     close(sockfd);
 
     return 0;
