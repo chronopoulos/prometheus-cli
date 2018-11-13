6c54f4020bfaa37a83b0d0260b5d9c27df87fdc0
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2018-11-12 23:59:13 -0500

    main.c: connect to multiple nodes using DNS resolution

--- src/main.c
+++ src/main.c
@@ -5,36 +5,72 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+
+#define NODE0_HOSTNAME "prometheusBone0.local"
+#define NODE0_PORTNO 50660
+
+#define NODE1_HOSTNAME "prometheusBone1.local"
+#define NODE1_PORTNO 50660
 
 
 int main(int argc, char **argv) {
 
-    // create socket
-    int sockfd;
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        fprintf(stderr, "ERROR: could not create socket\n");
+    // create sockets
+    int sock0, sock1;
+    sock0 = socket(AF_INET, SOCK_STREAM, 0);
+    sock1 = socket(AF_INET, SOCK_STREAM, 0);
+    if ((sock0 < 0) || (sock1 < 0)) {
+        fprintf(stderr, "ERROR: could not create sockets\n");
         return 1;
     }
 
-    // define connection address
-    struct sockaddr_in node_addr;
-    memset(&node_addr, '\0', sizeof(node_addr));
-    node_addr.sin_family = AF_INET;
-    node_addr.sin_port = htons(50660);
-    if(inet_pton(AF_INET, "10.0.5.253", &node_addr.sin_addr) <= 0) {
-        fprintf(stderr, "ERROR: invalid address\n");
+    // connect to node 0
+    struct sockaddr_in addr;
+    struct hostent *he;
+    struct in_addr **addr_list;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_port = htons(NODE0_PORTNO);
+    he = gethostbyname(NODE0_HOSTNAME);
+    if (he == NULL) {
+        fprintf(stderr, "ERROR: invalid hostname: %s\n", NODE0_HOSTNAME);
+        return 1;
+    } else {
+        addr.sin_family = he->h_addrtype;
+        addr_list = (struct in_addr **)he->h_addr_list;
+        if(inet_pton(he->h_addrtype, inet_ntoa(*addr_list[0]), &addr.sin_addr) <= 0) {
+            fprintf(stderr, "ERROR: inet_pton() failed\n");
+            return 1;
+        }
+    }
+    if(connect(sock0, (struct sockaddr *)&addr, sizeof(addr)) <= 0) {
+        fprintf(stderr, "ERROR: connect() failed\n");
         return 1;
     }
 
-    // make a connection
-    if(connect(sockfd, (struct sockaddr *)&node_addr, sizeof(node_addr)) < 0) {
+    // connect to node 1
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_port = htons(NODE1_PORTNO);
+    he = gethostbyname(NODE1_HOSTNAME);
+    if (he == NULL) {
+        fprintf(stderr, "ERROR: invalid hostname: %s\n", NODE1_HOSTNAME);
+        return 1;
+    } else {
+        addr.sin_family = he->h_addrtype;
+        addr_list = (struct in_addr **)he->h_addr_list;
+        if(inet_pton(he->h_addrtype, inet_ntoa(*addr_list[0]), &addr.sin_addr) <= 0) {
+            fprintf(stderr, "ERROR: inet_pton() failed\n");
+            return 1;
+        }
+    }
+    if(connect(sock0, (struct sockaddr *)&addr, sizeof(addr)) <= 0) {
         fprintf(stderr, "ERROR: connect() failed\n");
         return 1;
     }
 
     // prepare a message for the server
     char msgBuf[256];
-    int i,j;
     j = 0; // char index
     for (i=1; i<argc; i++) {
         strcpy(msgBuf + j, argv[i]);
@@ -45,22 +81,22 @@ int main(int argc, char **argv) {
     msgBuf[j] = 0x00; // null
 
     // send the message
-    send(sockfd, msgBuf, strlen(msgBuf), MSG_NOSIGNAL);
+    send(sock0, msgBuf, strlen(msgBuf), MSG_NOSIGNAL);
 
     // read length prefix
     uint32_t responseLength;
-    recv(sockfd, &responseLength, 4, 0);
+    recv(sock0, &responseLength, 4, 0);
 
     // read response
     unsigned char *responseBuf = (unsigned char *) malloc(responseLength);
-    recv(sockfd, responseBuf, responseLength, 0);
+    recv(sock0, responseBuf, responseLength, 0);
 
     // write binary data to stdout
     write(STDOUT_FILENO, responseBuf, responseLength);
 
     // clean up
     free(responseBuf);
-    close(sockfd);
+    close(sock0);
 
     return 0;
 
