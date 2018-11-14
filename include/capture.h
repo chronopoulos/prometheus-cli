754a80900fc2fbd2c591834813cd2da7d3b28c53
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2018-11-14 13:02:15 -0500

    complete handleCapture() with prime/collect functionality

--- include/capture.h
+++ include/capture.h
@@ -1,7 +1,11 @@
 #include <pigpio.h>
 #include <stdio.h>
 
+#include "network.h"
+
 #define SHUTTER_GPIO 21 // raspberry pi pin 40
 #define SHUTTER_PULSELEN_US 100
 
 void handleCapture(char*);
+int primeForCapture(void);
+void collectFromCapture(char*);
--- src/capture.c
+++ src/capture.c
@@ -2,20 +2,84 @@
 
 void handleCapture(char *cmd) {
 
+    // check if optarg begins with "get", exit if not
+    if (strncmp(cmd, "get", 3) != 0) {
+        fprintf(stderr, "ERROR: invalid capture request: %s\n", cmd);
+        return;
+    }
+
+    // prime
+    if (primeForCapture()) {
+        fprintf(stderr, "ERROR: primeForCapture failed\n");
+        return;
+    }
+
+    // trigger
     if(gpioInitialise() == PI_INIT_FAILED) {
 
         fprintf(stderr, "ERROR: gpioInitialise failed\n");
+        return;
 
     } else {
 
         if (gpioSetMode(SHUTTER_GPIO, PI_OUTPUT)) {
             fprintf(stderr, "ERROR: gpioSetMode failed\n");
+            return;
         }
 
         if (gpioTrigger(SHUTTER_GPIO, SHUTTER_PULSELEN_US, 1)) {
             fprintf(stderr, "ERROR: gpioTrigger failed\n");
+            return;
         }
 
     }
+
+    // collect
+    collectFromCapture(cmd+3);
+
+}
+
+int primeForCapture(void) {
+
+    int sock0 = connectToNode(NODE0_HOSTNAME, NODE0_PORTNO);
+
+    char primeMsg[256];
+    strcpy(primeMsg, "primeForCapture");
+    sendMessage(sock0, primeMsg);
+
+    uint32_t responseLength;
+    unsigned char* response = NULL;
+    responseLength = recv_lengthPrefixed(sock0, &response);
+
+    if (responseLength == 2) {
+        if ( (*(int16_t*)response) == 0 ) { // platform-dependent?
+
+            free(response);
+            close(sock0);
+            return 0;
+
+        }
+    }
+
+    return 1;
+
+}
+
+void collectFromCapture(char *cmd) {
+
+    int sock0 = connectToNode(NODE0_HOSTNAME, NODE0_PORTNO);
+
+    char collectMsg[256];
+    strcpy(collectMsg, "collect");
+    sendMessage(sock0, strcat(collectMsg,cmd));
+
+    uint32_t responseLength;
+    unsigned char* response = NULL;
+    responseLength = recv_lengthPrefixed(sock0, &response);
+
+    write(STDOUT_FILENO, response, responseLength);
+
+    free(response);
+    close(sock0);
 
 }
